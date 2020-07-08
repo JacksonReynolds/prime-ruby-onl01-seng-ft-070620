ccd26593bf6738e30d43879be5facfed422b48cc
Jackson Reynolds <jwreynold@gmail.com>
2020-07-08 14:28:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,19 @@
 # Add  code here!
 def prime?(int)
-  if int == 0 || int == 1
-    false
-  elsif int == 2
+  if int == 2 
     true
-  elsif int.even?
+  elsif int == 0 || int == 1 || int.even? || int % 3 == 0
     false
+  else
+    i = 5
+    while i**2 <= int
+      if int % i == 0 || int % (i+2) == 0
+        return false
+      end #if
+      i += 6
+    end #while
+    true
   end #if
+  
 
 end #prime?
